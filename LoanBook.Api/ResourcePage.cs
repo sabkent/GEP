3c4c670f11a22d23c9370c42a41bed3e9d0f7229
Sebastian Kent <sebastian.kent@memcf.co.uk>
2014-05-28 10:35:18 +0100

    WORK PC

---
+++ LoanBook.Api/ResourcePage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanBook.Api
+{
+    public class ResourcePage<T>
+    {
+        public Link Previous { get; set; }
+        public Link Next { get; set; }
+    }
+}
