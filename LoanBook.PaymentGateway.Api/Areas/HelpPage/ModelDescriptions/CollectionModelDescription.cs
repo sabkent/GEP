025612cae9b276c237f99fbb32c407889e467abb
Sebastian Kent <sebastian.kent@memcf.co.uk>
2014-06-17 14:51:02 +0100

    WORK PC

---
+++ LoanBook.PaymentGateway.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LoanBook.PaymentGateway.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ LoanBook.PaymentGateway.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LoanBook.PaymentGateway.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
